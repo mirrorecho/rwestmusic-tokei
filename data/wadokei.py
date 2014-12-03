4dd45251f49ec38080b36f4a8b6b3f04a55c3398
mirrorecho <info@randallwest.com>
2014-12-03 15:43:26 -0600

    wadokei start

---
+++ data/wadokei.py
@@ -0,0 +1,31 @@
+from abjad import *
+
+from arrangement import TokeiArrangement
+from cycles.loop import CycleLoop, Cycle
+from cycles.transform import *
+
+
+day_music = CycleLoop()
+night_music = CycleLoop()
+
+# to do... figure out how to add otoshi as a data point...
+
+# alternate between 3/4 and 6/8??
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(16)], start_iter=1)
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(8)], start_iter=1)
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(20)], start_iter=2)
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(4)], start_iter=2)
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(16)], start_iter=3)
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(8)], start_iter=3)
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(12)], start_iter=4)
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(12)], start_iter=4)
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(8)], start_iter=5)
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(16)], start_iter=5)
+
+day_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(4)])
+night_music.add_data("measures_durations", [durationtools.Duration(6,8) for i in range(20)])
