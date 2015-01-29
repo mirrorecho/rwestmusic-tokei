b8bf37698af644cf37276c4ec935ffc678d34340
mirrorecho <info@randallwest.com>
2015-01-29 01:51:30 -0600

    bub

--- bub.py
+++ bub.py
@@ -3,11 +3,14 @@ from wado_m import *
 from caes_m import *
 from kai_m import *
 
-# force_strings = ForceCloudStringsMelodyUp(name="caes-cloud-strings-melody-up")
-# force_strings.tally_loop()
+# f = ForceCloudStringsMelodyUp(name="caes-cloud-strings-melody-up")
+# f.tally_loop()
 
-f = ForceUp4BrassMelody(name="caes-cloud-highbrass-4-melody", autorandom=False)
+f= ForceMoveUp4WindsDown(name="caes-cloud-winds-up4-down")
 f.tally_loop()
+
+# f = ForceUp4BrassMelody(name="caes-cloud-highbrass-4-melody", autorandom=False)
+# f.tally_loop()
 
 #music.add_cycle(bubble_type=CaesiumMaterialOdd, flags=["2hits_a", "stop_class1", "melody"])
 
