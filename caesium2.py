5206869594269fae61f26577a697f44827c947c8
mirrorecho <info@randallwest.com>
2015-08-15 01:51:27 -0500

    more score template

--- caesium2.py
+++ caesium2.py
@@ -4,14 +4,20 @@ from calliope.bubbles import *
 from score import *
 
 
+class LineBoo(Line):
+    music = "g1"
 
 class CaesiumMusic(Bubble):
     flute1 = Line("b2")
+    flute2 = LineBoo()
     oboe2 = Line("a2")
 
 class CaesiumScore(TokeiScore):
     pass
 
-
+print("********************************")
 score = TokeiScore( CaesiumMusic() )
+# score = TokeiWinds( CaesiumMusic() )
+# score = TokeiFlutes( CaesiumMusic() )
 score.show()
+# print(score)
--- score.py
+++ score.py
@@ -3,14 +3,29 @@ from abjad import *
 from calliope.bubbles import *
 
 
-class TokeiFlutes(BubbleStaffGroup):
+class TokeiFlutes(InstrumentStaffGroup):
     flute1 = BubbleStaff(instrument=instrumenttools.Flute(instrument_name="Flute 1", short_instrument_name="fl.1"))
     flute2 = BubbleStaff(instrument=instrumenttools.Flute(instrument_name="Flute 2", short_instrument_name="fl.2"))
 
-class TokeiOboes(BubbleStaffGroup):
+class TokeiOboes(InstrumentStaffGroup):
     oboe1 = BubbleStaff(instrument=instrumenttools.Oboe(instrument_name="Oboe 1", short_instrument_name="ob.1"))
     oboe2 = BubbleStaff(instrument=instrumenttools.Oboe(instrument_name="Oboe 2", short_instrument_name="ob.2"))
 
-class TokeiScore(BubbleScore):
+class TokeiClarinets(InstrumentStaffGroup):
+    clarinet1 = BubbleStaff(instrument=instrumenttools.ClarinetInBFlat(instrument_name="Clarinet 1", short_instrument_name="cl.1"))
+    clarinet2 = BubbleStaff(instrument=instrumenttools.ClarinetInBFlat(instrument_name="Clarinet 2", short_instrument_name="cl.2"))
+
+class TokeiBassoons(InstrumentStaffGroup):
+    bassoon1 = BubbleStaff(instrument=instrumenttools.Oboe(instrument_name="Bassoon 1", short_instrument_name="bsn.1"))
+    bassoon2 = BubbleStaff(instrument=instrumenttools.Oboe(instrument_name="Bassoon 2", short_instrument_name="bsn.2"))
+
+class TokeiWinds(BubbleStaffGroup):
     flutes = TokeiFlutes()
     oboes = TokeiOboes()
+    clarinets = TokeiClarinets()
+    bassoons = TokeiBassoons()
+
+    sequence = ("flutes","oboes", "clarinets", "bassoons")
+
+class TokeiScore(BubbleScore):
+    winds = TokeiWinds()
